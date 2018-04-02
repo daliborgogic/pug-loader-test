83443f4ea7233544cdbc948944da1c3260e8faf1
Dalibor Gogic <mail@daliborgogic.com>
2018-04-02 15:58:13 +0200

    unused package removed

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,5 @@
 <template lang="pug">
-.view
+div
   h1 Hello there
   p Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo earum, nihil commodi eum aspernatur eius est dolorem enim soluta. Non!
 </template>
-
-<style lang="stylus" scoped>
-// @import '~@/assets/variables.styl'
-
-.view
-  max-width 512px
-  padding-left 1rem
-  padding-right 1rem
-  margin 0 auto
-  color $red
-</style>
